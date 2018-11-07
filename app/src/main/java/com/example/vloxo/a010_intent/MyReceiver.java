33fa945a775f3db15f4635aeb49e1676d8ce2abd
Max <vloxomire@gmail.com>
2018-11-07 02:02:15 -0300

    widget terminado

---
+++ app/src/main/java/com/example/vloxo/a010_intent/MyReceiver.java
@@ -0,0 +1,14 @@
+package com.example.vloxo.a010_intent;
+
+import android.appwidget.AppWidgetProvider;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MyReceiver extends AppWidgetProvider {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
